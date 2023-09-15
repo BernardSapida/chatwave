99f91265905023565799a336a8df447575a0e566
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-09-15 14:39:08 +0800

    websocket test

---
+++ src/components/Sample.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import { pusherClient } from '@/lib/pusher'
+
+export default function Sample() {
+    const [number, setNumber] = useState<number>(0);
+
+    useEffect(() => {
+        pusherClient.subscribe('subscribeFromClient')
+        pusherClient.bind('myEvent', changesHandler)
+
+        return () => {
+            pusherClient.unsubscribe('subscribeFromClient')
+            pusherClient.unbind('myEvent', changesHandler)
+        }
+    }, [number]);
+
+    const changesHandler = (num: number) => {
+        setNumber(prev => prev + num)
+    }
+
+    const sendMessage = async () => {
+        const res = await axios.get('/api/sample');
+        console.log(res.data.name)
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <button onClick={sendMessage}>Send {number}</button>
+        </main>
+    )
+}
